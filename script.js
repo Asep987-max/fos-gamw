f94269ec313b3662214342a328b840cc791cf1cb
Asep987-max <h319417@gmail.com>
2025-04-25 00:29:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Script untuk Toggler Menu Mobile
- const mobileMenuButton = document.getElementById('mobile-menu-button');
- const mobileMenu = document.getElementById('mobile-menu');
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
 
- mobileMenuButton.addEventListener('click', () => {
-     mobileMenu.classList.toggle('hidden');
- });
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
 
- // Script untuk menutup menu mobile saat link di klik (opsional)
- const mobileMenuLinks = mobileMenu.querySelectorAll('a');
- mobileMenuLinks.forEach(link => {
-     link.addEventListener('click', () => {
-         mobileMenu.classList.add('hidden');
-     });
- });
+// Script untuk menutup menu mobile saat link di klik (opsional)
+const mobileMenuLinks = mobileMenu.querySelectorAll('a');
+mobileMenuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.add('hidden');
+    });
+});
 
- // Script untuk menampilkan tahun saat ini di footer
- document.getElementById('tahun').textContent = new Date().getFullYear();
+// Script untuk menampilkan tahun saat ini di footer
+document.getElementById('tahun').textContent = new Date().getFullYear();
